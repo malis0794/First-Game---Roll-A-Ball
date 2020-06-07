627491bdf627eeaea190a87fca825b9643f7bbe4
Muhammad Ali <muhammadali007ninja@gmail.com>
2020-06-07 16:45:27 +0100

    Add files via upload

---
+++ Assets/Scripts/Rotater.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotater : MonoBehaviour {
+
+
+	void Update () 
+	{
+		transform.Rotate (new Vector3 (30, 60, 90) * Time.deltaTime);
+	}
+}
